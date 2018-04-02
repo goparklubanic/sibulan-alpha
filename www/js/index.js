5561d580f9915891bec6c957a66be6d55116d148
goparklubanic <ngoparios@yahoo.com>
2018-04-02 09:10:12 +0700

    initiate commit

---
+++ www/js/index.js
@@ -0,0 +1,79 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+    //    app.receivedEvent('deviceready');
+        app.setupPush();
+    },
+
+
+    // New application Push setup function
+    setupPush: function() {
+
+    //Initialise Push Notifications
+    var push = PushNotification.init({
+      "android": {},
+      "ios": {
+      "sound": true,
+      "alert": true,
+      "badge": true
+      },
+      "windows": {}
+    });
+
+  //Function that executes once registered with Firebase
+    push.on('registration', function(data) {
+    //  console.log("registration event: " + data.registrationId);
+    //  console.log("device type: " + device.platform);
+    //  var info = data.registrationId+", "+device.platform;
+      document.getElementById('regId').innerHTML = data.registrationId;
+      // document.getElementById('platform').innerHTML = device.platform;
+      // document.write('<div>' + data.registrationId + '</div>');
+  });
+
+  //Error handler
+  push.on('error', function(e) {
+  //  console.log("push error: " + e.message);
+    document.getElementById('status').innerHTML = "error: "+e.message;
+  });
+
+  //Function that executes when a notification is received
+  push.on('notification', function(data) {
+    // console.log('notification event received');
+    var pesan = data.message +'<br/>'+data.title;
+    document.getElementById('status').innerHTML = 'notification event received';
+    document.getElementById('pesan').innerHTML = pesan;
+    /*
+    navigator.notification.alert(
+    data.message, // message
+    null, // callback
+    data.title, // title
+    'Ok' // buttonName
+    );
+    */
+  });
+
+  //Push finish setup function
+  push.finish(function() {
+    // console.log('Push Setup Success');
+    document.getElementById('status').innerHTML = "Push Success";
+    }, function() {
+    // console.log('Push Setup Error');
+    document.getElementById('status').innerHTML = "Push Failed";
+  });
+
+  }
+};
