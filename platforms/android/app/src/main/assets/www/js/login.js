93e29b160a0fec0839a7d4698b90eaa0493f1d85
goparklubanic <ngoparios@yahoo.com>
2018-04-04 09:27:17 +0700

    hide debug divs

---
+++ platforms/android/app/src/main/assets/www/js/login.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  var remoteSite = 'https://klubaners.web.id/sibulan/resi/drivers.php';
+  $("#submit").click( function(){
+
+    var nmTelp = $("#nmTelp").val();
+    var password = $("#password").val();
+
+    $.post(remoteSite+'?tes=login',
+      { nmTelp: nmTelp, password: password },
+      function(response){
+        var driver = JSON.parse(response);
+        console.log(driver);
+        //$("#noTelp").html(driver.nmTelp);
+        localStorage.setItem('nmTelp',driver.nmTelp);
+        window.location='index.html';
+      }
+    );
+
+  });
+
+});
