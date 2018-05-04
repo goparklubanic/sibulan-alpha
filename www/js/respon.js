cea9a0f9de4803306daea19bae120fcc9a954edd
goparklubanic <ngoparios@yahoo.com>
2018-05-04 09:44:52 +0700

    change style, add respon map

--- www/js/respon.js
+++ www/js/respon.js
@@ -2,9 +2,16 @@ $(document).ready( function(){
   var pemohon     = localStorage.getItem('jobPemohon');
   var jemput      = localStorage.getItem('jobAlamat');
   var tujuan      = localStorage.getItem('jobTujuan');
-
+  var geoloc      = localStorage.getItem('jemput');
+  var jmpLink     = "<a href=# onClick=openmap('"+geoloc+"')>"+jemput+"</a>";
   $("#pemohon").html(pemohon);
-  $("#jemput").html(jemput);
+  $("#jemput").html(jmpLink);
   $("#tujuan").html(tujuan);
 
+
 });
+
+function openMap(geocoords){
+  var label = encodeURI('Titik Jemput'); // encode the label!
+  window.open('geo:0,0?q=' + geocoords + '(' + label + ')', '_system');
+}
