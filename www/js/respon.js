a0297cdc76885c5e0e71797b81b65fbad10e9d8d
goparklubanic <ngoparios@yahoo.com>
2018-05-16 12:40:29 +0700

    add responer to dbs

--- www/js/respon.js
+++ www/js/respon.js
@@ -4,6 +4,7 @@ $(document).ready( function(){
   var tujuan      = localStorage.getItem('jobTujuan');
   var geoloc      = localStorage.getItem('jemput');
   var jmpLink     = "<a href=# onClick=openMap('"+geoloc+"')>"+jemput+"</a>";
+  $("#ticketId").html(localStorage.getItem('ticketId'));
   $("#pemohon").html(pemohon);
   $("#jemput").html(jmpLink);
   $("#tujuan").html(tujuan);
